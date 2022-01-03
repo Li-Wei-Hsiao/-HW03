965500c331f8c66c3c36f253c0e3f7f6b8956c08
Li-Wei-Hsiao <80880278+Li-Wei-Hsiao@users.noreply.github.com>
2022-01-03 20:55:26 +0800

    Add files via upload

---
+++ HW03_09156242/HW03_09156242/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace HW03_09156242
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.display_box = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.display_box)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // display_box
+            // 
+            this.display_box.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.display_box.Location = new System.Drawing.Point(67, 63);
+            this.display_box.Name = "display_box";
+            this.display_box.Size = new System.Drawing.Size(500, 714);
+            this.display_box.TabIndex = 0;
+            this.display_box.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox1.Location = new System.Drawing.Point(660, 244);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(108, 28);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Tag = "1";
+            this.checkBox1.Text = "永恆族";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox2.Location = new System.Drawing.Point(660, 283);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(308, 28);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Tag = "2";
+            this.checkBox2.Text = "刀劍神域-無星夜的詠嘆調";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox3.Location = new System.Drawing.Point(660, 323);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(284, 28);
+            this.checkBox3.TabIndex = 1;
+            this.checkBox3.Tag = "3";
+            this.checkBox3.Text = "惡靈古堡首部曲-拉昆市";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox4.Location = new System.Drawing.Point(660, 364);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(132, 28);
+            this.checkBox4.TabIndex = 1;
+            this.checkBox4.Tag = "4";
+            this.checkBox4.Text = "魔法滿屋";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox5.Location = new System.Drawing.Point(660, 406);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(84, 28);
+            this.checkBox5.TabIndex = 1;
+            this.checkBox5.Tag = "5";
+            this.checkBox5.Text = "詭扯";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(591, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(311, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "請選擇要播放海報：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(632, 464);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(324, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "請輸入每張海報的播放間隔：";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(740, 523);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "秒";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(679, 523);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(636, 629);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(284, 43);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "套用";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.apply);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 852);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.display_box);
+            this.Name = "Form1";
+            this.Text = "HW03";
+            ((System.ComponentModel.ISupportInitialize)(this.display_box)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox display_box;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ HW03_09156242/HW03_09156242/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW03_09156242
+{
+    public partial class Form1 : Form
+    {
+        double second;
+        int index = 0;
+        List<CheckBox> pictures = new List<CheckBox>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void apply(object sender, EventArgs e)
+        {
+            Boolean k = true;
+            if (timer1.Enabled == true)
+                timer1.Stop();
+
+            if (textBox1.Text.Equals(""))
+            {
+                second = 1.0;
+                k = false;
+            }
+            else
+                second = Convert.ToDouble(textBox1.Text);
+
+            pictures.Clear();
+            check_and_add();
+            timer1.Interval = (int)(second * 1000);
+            if(pictures.Count > 0 && k)
+                timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            String name = "movie_00" + pictures[index].Tag;
+            display_box.BackgroundImage = (Image)Properties.Resources.ResourceManager.GetObject(name);
+            if (index + 1 == pictures.Count())
+                index = 0;
+            else
+                index++;
+        }
+
+        void check_and_add()
+        {
+            if (checkBox1.Checked == true)
+                pictures.Add(checkBox1);
+            if (checkBox2.Checked == true)
+                pictures.Add(checkBox2);
+            if (checkBox3.Checked == true)
+                pictures.Add(checkBox3);
+            if (checkBox4.Checked == true)
+                pictures.Add(checkBox4);
+            if (checkBox5.Checked == true)
+                pictures.Add(checkBox5);
+        }
+    }
+}
